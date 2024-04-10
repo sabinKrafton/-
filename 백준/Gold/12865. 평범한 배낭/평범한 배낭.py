42478628c265679eb229be2f3c4592acbc780e70
sabinKrafton <160460876+sabinKrafton@users.noreply.github.com>
2024-04-10 16:03:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 5268 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+
+bag = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
+
+DP = [[0] * (K + 1) for _ in range(N + 1)]
+
+
+for i in range(1, N + 1): # i는 물건 번호
+    for j in range(1, K + 1): # j는 가방 무게
+        if bag[i][0] > j:
+            DP[i][j] = DP[i - 1][j]
+        else:
+            DP[i][j] = max(DP[i - 1][j], DP[i - 1][j-bag[i][0]] + bag[i][1])
+# print(DP)
+print(DP[-1][-1])
